7acbcb425ab4cd132ea83938e88b6225c88f2031
Samson <51507673+MrSamafu@users.noreply.github.com>
2022-08-14 15:48:49 +0200

    Update front

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "904ce63d0352a8fe"; }
+/******/ 		__webpack_require__.h = function() { return "a802bb755109511b"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
---
+++ pages/createRecipe/index.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+
+const CreateRecipe: NextPage = () => {
+    return (
+        <>
+        </>
+    )
+}
+
+export default CreateRecipe;
